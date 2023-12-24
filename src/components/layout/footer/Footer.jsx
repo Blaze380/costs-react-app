18360494da7f865598d6d4656c137ed413d18202
Blaze380 <144695514+Blaze380@users.noreply.github.com>
2023-12-24 19:07:15 +0200

    Style: Adding Footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,8 +1,15 @@
-//import styles from './Footer.module.css';
-
+import styles from './Footer.module.css';
+import { FaFacebook, FaWhatsapp, FaGithub } from 'react-icons/fa';
 function Footer() {
     return (
-        <p>Gabadiya</p>
+        <footer className={styles.footer}>
+            <ul>
+                <li className={`${styles.social_list}  ${styles.facebook_logo}`} ><FaFacebook/></li>
+                <li className={`${styles.social_list}  ${styles.whatsapp_logo}`}><FaWhatsapp/></li>
+                <li className={`${styles.social_list}  ${styles.github_logo}`}><FaGithub/></li>
+            </ul>
+            <p><span>Blazeland</span> &copy; 2023</p>
+        </footer>
     );
 }
 export default Footer;
