8d81a0859bde18644069fc3e610049343cd951f3
Blaze380 <144695514+Blaze380@users.noreply.github.com>
2023-12-24 19:29:28 +0200

    Style: Finished treating Footer In the footer as responsivity but

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,14 +1,15 @@
 import styles from './Footer.module.css';
 import { FaFacebook, FaWhatsapp, FaGithub } from 'react-icons/fa';
 function Footer() {
+    const socialIconsSize = 25;
     return (
         <footer className={styles.footer}>
             <ul>
-                <li className={`${styles.social_list}  ${styles.facebook_logo}`} ><FaFacebook/></li>
-                <li className={`${styles.social_list}  ${styles.whatsapp_logo}`}><FaWhatsapp/></li>
-                <li className={`${styles.social_list}  ${styles.github_logo}`}><FaGithub/></li>
+                <li className={`${styles.social_list}  ${styles.facebook_logo}`} ><FaFacebook size={socialIconsSize}/></li>
+                <li className={`${styles.social_list}  ${styles.whatsapp_logo}`}><FaWhatsapp size={socialIconsSize}/></li>
+                <li className={`${styles.social_list}  ${styles.github_logo}`}><FaGithub size={socialIconsSize}/></li>
             </ul>
-            <p><span>Blazeland</span> &copy; 2023</p>
+            <p className={styles.copy_right}><span className={styles.copy_right_span}>Blazeland</span> &copy; 2023</p>
         </footer>
     );
 }
